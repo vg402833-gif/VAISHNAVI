46e3aaf0c4af7483b18ea31221c2e54992ed8e48
VAISHNAVI <vg402833@gmail.com>
2025-12-02 19:55:43 +0530

    Add files via upload

---
+++ mathematical operations.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("The addition of two numbers:",add)
+print("Substraction of two numbers",sub)
+print("The multiplication of two numbers",mul)
+print("The division of two numbers:",div)
