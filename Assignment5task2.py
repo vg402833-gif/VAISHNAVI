bba5bc75296517f0dbb1779d611f2eb47e768889
VAISHNAVI <vg402833@gmail.com>
2026-03-01 21:06:07 +0530

    Add files via upload

---
+++ Assignment5task2.py
@@ -0,0 +1,6 @@
+list1=[1,2,3,4,5,6,7,8,9,10] # list of numbers
+print(f"Original list:{list1}") # printing hte original list
+list2=list1[0:5]
+print(f"Extracted first five elements:{list2}") # printing first five elements
+list2.reverse()
+print(f"Reversed extracted elements:{list2}") # prints reverse of extracted list
